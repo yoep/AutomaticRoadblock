335c6d5c16d7eff5bca7b4eb5be8601e347e63da
yoep <yoeri.peeters10@telenet.be>
2022-08-31 12:21:56 +0200

    #14: Fixed issue with the logging level

--- AutomaticRoadblock/AbstractionLayer/LogLevel.cs
+++ AutomaticRoadblock/AbstractionLayer/LogLevel.cs
@@ -5,17 +5,17 @@ namespace AutomaticRoadblocks.AbstractionLayer
     [Flags]
     public enum LogLevel
     {
-        // 0001 0000
-        Trace = 16,
+        // 0001 1111
+        Trace = 31,
 
-        // 0000 1000
-        Debug = 8,
+        // 0000 1111
+        Debug = 15,
 
-        // 0000 0100
-        Info = 4,
+        // 0000 0111
+        Info = 7,
 
-        // 0000 0010
-        Warn = 2,
+        // 0000 0011
+        Warn = 3,
 
         // 0000 0001
         Error = 1,
