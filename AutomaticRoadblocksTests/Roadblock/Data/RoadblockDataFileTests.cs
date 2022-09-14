f03fad2e46aec8d6ff9323a43bdd15b436d8703d
yoep <yoeri.peeters10@telenet.be>
2022-09-14 21:49:08 +0200

    #39: Fixed unit test of roadblock data file

--- AutomaticRoadblocksTests/Roadblock/Data/RoadblockDataFileTests.cs
+++ AutomaticRoadblocksTests/Roadblock/Data/RoadblockDataFileTests.cs
@@ -16,7 +16,7 @@ namespace AutomaticRoadblocks.Roadblock.Data
         public void TestRoadblockDeserialization()
         {
             var expectedLevel1Result = new RoadblockData(1, "small_cone_stripes");
-            var expectedLevel4Result = new RoadblockData(4, "police_do_not_cross", "work_barrier_high", "barrel_traffic_catcher");
+            var expectedLevel4Result = new RoadblockData(5, "police_do_not_cross", "work_barrier_high", "barrel_traffic_catcher");
             var data = IoC.Instance.GetInstance<IRoadblockData>();
 
             var result = data.Roadblocks;
