3249775fd705760afb85fed77363e25d099907fb
yoep <yoeri.peeters10@telenet.be>
2022-09-23 20:27:38 +0200

    #12: Fixed API issue of method not being static

--- AutomaticRoadblock/Api/Functions.cs
+++ AutomaticRoadblock/Api/Functions.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics.CodeAnalysis;
+using AutomaticRoadblocks.AbstractionLayer;
 using AutomaticRoadblocks.Data;
 using AutomaticRoadblocks.ManualPlacement;
 using AutomaticRoadblocks.Models;
@@ -32,16 +33,29 @@ namespace AutomaticRoadblocks.Api
         /// Get the traffic redirection dispatcher.
         /// </summary>
         public static IRedirectTrafficDispatcher RedirectTrafficDispatcher => IoC.Instance.GetInstance<IRedirectTrafficDispatcher>();
-        
+
         /// <summary>
         /// Get the spike strip dispatcher instance.
         /// </summary>
         public static ISpikeStripDispatcher SpikeStripDispatcher => IoC.Instance.GetInstance<ISpikeStripDispatcher>();
-        
+
         /// <summary>
         /// Get the model provider instance.
         /// This provider converts the raw data from <see cref="IDataFile"/> to actual models.
         /// </summary>
         public static IModelProvider ModelProvider => IoC.Instance.GetInstance<IModelProvider>();
+
+        /// <summary>
+        /// Dispatch a roadblock for the current pursuit.
+        /// This method has been introduced for Grammar Police and is the same as using <code>PursuitManager.DispatchNow(xxx)</code>.
+        /// </summary>
+        /// <param name="userRequested">Indicates if the roadblock was called by the user.</param>
+        /// <returns>Returns true if a roadblock is dispatched, else false.</returns>
+        public static bool DispatchNow(bool userRequested = false)
+        {
+            var logger = IoC.Instance.GetInstance<ILogger>();
+            logger.Debug($"Dispatching roadblock from functions API with {nameof(userRequested)}: {userRequested}");
+            return PursuitManager.DispatchNow(userRequested);
+        }
     }
 }
