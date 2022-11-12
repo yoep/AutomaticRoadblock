35d3adc1ee8d5f8feee4a21199f10467a5e7c7c8
yoep <yoeri.peeters10@telenet.be>
2022-11-12 12:33:07 +0100

    #118: Added clean all ini config

--- AutomaticRoadblock/Settings/GeneralSettings.cs
+++ AutomaticRoadblock/Settings/GeneralSettings.cs
@@ -10,6 +10,14 @@ namespace AutomaticRoadblocks.Settings
         public Keys OpenMenuModifierKey { get; internal set; }
         
         /// <summary>
+        /// Clean all roadblocks, manual placements and traffic redirections
+        /// This will remove all scenery items and units created by the plugin
+        /// </summary>
+        public Keys CleanAllKey { get; internal set; }
+        
+        public Keys CleanAllModifierKey { get; internal set; }
+        
+        /// <summary>
         /// The log level of the plugin
         /// This will decide the log verbosity of the plugin to the log file RagePluginHook.log
         /// </summary>
