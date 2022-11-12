a839386d159f2f9400b92166c5c7fe347648eeb0
yoep <yoeri.peeters10@telenet.be>
2022-11-12 11:47:17 +0100

    #115: Removed road info debug component as the same functionality is provided by the road preview

--- AutomaticRoadblock/Main.cs
+++ AutomaticRoadblock/Main.cs
@@ -262,7 +262,6 @@ namespace AutomaticRoadblocks
                 .Register<IMenuSwitchItem>(typeof(DebugMenuSwitchItem))
                 .Register<IMenuComponent<UIMenuItem>>(typeof(DebugPursuitToggleComponent))
                 .Register<IMenuComponent<UIMenuItem>>(typeof(DebugPursuitForceOnFootComponent))
-                .Register<IMenuComponent<UIMenuItem>>(typeof(DebugRoadInfoComponent))
                 .Register<IMenuComponent<UIMenuItem>>(typeof(DebugRoadPreviewComponent))
                 .Register<IMenuComponent<UIMenuItem>>(typeof(DebugDeploySpikeStripComponent))
                 .Register<IMenuComponent<UIMenuItem>>(typeof(DebugPreviewSpikeStripComponent))
